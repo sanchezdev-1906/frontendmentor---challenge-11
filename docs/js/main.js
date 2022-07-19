ba8d6a8c8dd46143a7db86a31ecab6f2f73c43a5
Joel Sanchez <joelalejandro.development@gmail.com>
2022-07-19 17:04:38 -0400

    json data added to main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,13 +2,119 @@ const options = document.getElementById("options");
 const cards = document.getElementById("cards");
 let selectedOption = options.children[1];
 
-const getData = async () => 
-{
-  const json = await fetch("../data/data.json")
-  .then(res => res.json())
-  .then(res => res)
-  return await json;
-}
+// ! This method doesn't work in github pages
+// const getData = async () => 
+// {
+//   const json = await fetch("../data/data.json")
+//   .then(res => res.json())
+//   .then(res => res)
+//   return await json;
+// }
+
+const dataJSON = [
+  {
+    "title": "Work",
+    "timeframes": {
+      "daily": {
+        "current": 5,
+        "previous": 7
+      },
+      "weekly": {
+        "current": 32,
+        "previous": 36
+      },
+      "monthly": {
+        "current": 103,
+        "previous": 128
+      }
+    }
+  },
+  {
+    "title": "Play",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 2
+      },
+      "weekly": {
+        "current": 10,
+        "previous": 8
+      },
+      "monthly": {
+        "current": 23,
+        "previous": 29
+      }
+    }
+  },
+  {
+    "title": "Study",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 7
+      },
+      "monthly": {
+        "current": 13,
+        "previous": 19
+      }
+    }
+  },
+  {
+    "title": "Exercise",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 5
+      },
+      "monthly": {
+        "current": 11,
+        "previous": 18
+      }
+    }
+  },
+  {
+    "title": "Social",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 3
+      },
+      "weekly": {
+        "current": 5,
+        "previous": 10
+      },
+      "monthly": {
+        "current": 21,
+        "previous": 23
+      }
+    }
+  },
+  {
+    "title": "Self Care",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 2,
+        "previous": 2
+      },
+      "monthly": {
+        "current": 7,
+        "previous": 11
+      }
+    }
+  }
+]
 
 options.addEventListener("click", (e)=>
 {
@@ -24,7 +130,7 @@ options.addEventListener("click", (e)=>
 
 const changeData = async (time = "weekly") =>
 {
-  const data = await getData()
+  const data = dataJSON
   let timeText = "Week";
   time === "weekly"? timeText = "Week" : time === "daily"? timeText = "Day": timeText = "Month";
 
